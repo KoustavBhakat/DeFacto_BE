fd29496059f4fcfa29828022c521e3af5c237a20
LRM-00059\Koustav Bhakat <koustav.bhakat@leftrightmind.com>
2023-08-11 16:18:20 +0530

    Base framework setup

---
+++ src/main/java/com/lrm/defacto/backend/DefactoBeApplication.java
@@ -0,0 +1,13 @@
+package com.lrm.defacto.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefactoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefactoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lrm/defacto/backend/DefactoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrm.defacto.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefactoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
